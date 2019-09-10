a9508b5142469a02f6c8ccaa07474c4acf6d61ae
Rushil Shakya <rushil.shakya@gmail.com>
2019-09-10 17:06:03 -0400

    encrypted password in db

--- server/index.js
+++ server/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const passport = require('passport');
 
 const { db } = require('./db');
+
+const dbStore = new SequelizeStore({ db: db });
+dbStore.sync();
 
 const app = express();
 const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
@@ -13,6 +19,16 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'a wildly insecure secret',
+    store: dbStore,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //routing
 app.use('/api', require('./api'));
@@ -25,6 +41,19 @@ app.use(function(err, req, res, next) {
   console.error(err);
   console.error(err.stack);
   res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+//passport stuff
+passport.serializeUser((user, done) => {
+  try {
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
 });
 
 //lets get started
