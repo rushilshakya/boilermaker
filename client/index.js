930444c04d08dd780004a3873e1e98dfc5381c6a
Rushil Shakya <rushil.shakya@gmail.com>
2019-09-10 15:02:25 -0400

    redux configured

--- client/index.js
+++ client/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 ReactDOM.render(
-  <div>Hello, world buddy!</div>,
+  <Provider store={store}>
+    <div>Hello, world buddy!</div>
+  </Provider>,
   document.getElementById('app') // make sure this is the same as the id of the div in your index.html
 );
---
+++ client/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+import reducer from './reducers';
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunkMiddleware, createLogger())
+);
+
+export default store;
