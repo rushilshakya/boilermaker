5f3f30f8e468e53d3fc10b654efd649396ad073f
Rushil Shakya <rushil.shakya@gmail.com>
2019-09-10 23:22:54 -0400

    local user management

--- server/api/auth.js
+++ server/api/auth.js
@@ -6,7 +6,15 @@ router.get('/', (req, res, next) => {
   res.send('hola bird');
 });
 
+// /api/auth/me
+// to get an in session user
+router.get('/me', (req, res, next) => {
+  if (req.user) res.json(req.user);
+  else res.status(404).send('no me');
+});
+
 // /api/auth/login
+// user login
 router.put('/login', async (req, res, next) => {
   try {
     const user = await User.findOne({ where: { email: req.body.email } });
@@ -24,9 +32,29 @@ router.put('/login', async (req, res, next) => {
   }
 });
 
-router.get('/me', (req, res, next) => {
-  if (req.user) res.json(req.user);
-  else res.status(404).send('no me');
+// /api/auth/login
+// create account
+router.post('/signup', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (user) res.status(409).send('User already exists');
+    const newUser = await User.create(req.body);
+    if (newUser) {
+      req.logIn(newUser, err => {
+        if (err) next(err);
+        else res.status(201).json(newUser);
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// /api/auth/logout
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  req.session.destroy();
+  res.sendStatus(204);
 });
 
 module.exports = router;
