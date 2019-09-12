95c5d43a3a5f3528d179eeac02e6e950e1e96e99
Rushil Shakya <rushil.shakya@gmail.com>
2019-09-11 20:53:22 -0400

    adding a sample localSecrets file

---
+++ localSecrets.sample.js
@@ -0,0 +1,8 @@
+//rename this file to "localSecrets.js"
+//fill in the correct values as per your settings
+//DO NOT CHECKIN your "localSecrets.js" file
+process.env.GOOGLE_CLIENT_ID = '';
+process.env.GOOGLE_CLIENT_SECRET = '';
+process.env.DATABASE_URL = '';
+process.env.PORT = 3000;
+process.env.SESSION_SECRET = '';
